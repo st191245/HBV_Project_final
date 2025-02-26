7040e350f2ac38164c388af6261f423e178fc7dd
ShunmugaPriya <sspriya1118@gmail.com>
2025-02-27 03:10:33 +0530

    Update main.py

--- HBV/main.py
+++ HBV/main.py
@@ -2,7 +2,7 @@ from HBV_Core.runoff import SoilMoisture
 from HBV_Core.visualization import *
 from HBV_Core.log_config import *
 from HBV_Core.user_config import *
-
+#Authors: BKS
 
 def hbv_calculation():
     """
